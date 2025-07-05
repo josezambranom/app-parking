08f754708752b94726ef419b6088805e4c7d1a12
José Zambrano <josezambranomacias01@gmail.com>
2025-07-05 11:58:54 -0500

    first commit

---
+++ app/src/test/java/com/smartparking/app/SmartparkingappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smartparking.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartparkingappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
